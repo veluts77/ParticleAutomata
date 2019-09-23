d4d33ccc56cd3c125350a1a0ce66114615efac16
vlutsenk <vladimir.lutsenko@t-systems.ru>
2019-09-23 18:32:54 +0300

    Tornado FX experiments

--- fxui/src/main/kotlin/ui/ParticleApp.kt
+++ fxui/src/main/kotlin/ui/ParticleApp.kt
@@ -1,9 +1,32 @@
 package ui
 
+import javafx.scene.Group
+import javafx.stage.Stage
 import tornadofx.App
 import tornadofx.launch
+import javafx.scene.Scene
+import java.awt.Color
+import com.sun.javafx.robot.impl.FXRobotHelper.getChildren
+import javafx.scene.paint.Paint
+import javafx.scene.shape.Rectangle
 
-class ParticleApp : App(MainView::class)
+
+class ParticleApp : App(MainView::class) {
+    override fun start(stage: Stage) {
+        //super.start(stage)
+        val root = Group()
+
+        val scene = Scene(root, 500.0, 500.0, true)
+
+        val r = Rectangle(25.0, 25.0, 250.0, 250.0)
+        r.setFill(Paint.valueOf("blue"))
+        root.children.add(r)
+
+        stage.title = "JavaFX Scene Graph Demo"
+        stage.scene = scene
+        stage.show()
+    }
+}
 
 fun launchApp() {
     launch<ParticleApp>()
--- main/src/main/kotlin/Main.kt
+++ main/src/main/kotlin/Main.kt
@@ -1,7 +1,10 @@
 import ui.Form
+import ui.launchApp
 
 fun main() {
-    val f = Form()
-    f.title = "Kotlin Objects"
-    Thread(f).start()
+//    val f = Form()
+//    f.title = "Kotlin Objects"
+//    Thread(f).start()
+
+    launchApp()
 }
