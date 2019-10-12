c51731ec4d3502916e35198dc3d3aa37a0dab213
veluts <veluts77@gmail.com>
2019-10-12 23:50:22 +0300

    playing with javafx canvas

--- fxui/src/main/kotlin/ui/MainView.kt
+++ fxui/src/main/kotlin/ui/MainView.kt
@@ -1,9 +1,15 @@
 package ui
 
+import javafx.scene.canvas.Canvas
+import javafx.scene.image.WritableImage
 import javafx.scene.paint.Color
 import tornadofx.*
 
 class MainView : View() {
+
+    val writableImage = WritableImage(100, 100)
+    var cnv: Canvas = Canvas(1.0, 1.0)
+
     override val root = pane {
         setMinSize(500.0, 500.0)
         title = "Particle Automata"
@@ -16,6 +22,47 @@ class MainView : View() {
                 height = 250.0
                 fill = Color.BEIGE
             }
+
+            imageview {
+                x = 50.0
+                y = 50.0
+                image = writableImage
+
+            }
+
+            cnv = canvas {
+                width = 100.0
+                height = 100.0
+                layoutX = 100.0
+                layoutY = 100.0
+            }
         }
+
+        runAsync {
+//            while (true) {
+                writableImage.pixelWriter.setColor(
+                        randomX(),
+                        randomY(),
+                        Color.BLACK
+                )
+
+                with(cnv.graphicsContext2D) {
+                    fill = Color.AZURE
+                    fillRect(0.0, 0.0, 99.0, 99.0)
+                    rect(0.0, 0.0, 99.0, 99.0)
+                    fill()
+                }
+
+                try {
+                    Thread.sleep(15)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+            }
+//        }
     }
+
+    private fun randomY() = Math.round(Math.random() * (writableImage.height - 1)).toInt()
+
+    private fun randomX() = Math.round(Math.random() * (writableImage.width - 1)).toInt()
 }
