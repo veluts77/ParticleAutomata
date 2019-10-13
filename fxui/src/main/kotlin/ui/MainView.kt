c12654a32a932d6d631622a8dd1fff54fa4c7c84
veluts <veluts77@gmail.com>
2019-10-14 00:25:04 +0300

    particles are rendered now but sometimes rendering stops

--- fxui/src/main/kotlin/ui/MainView.kt
+++ fxui/src/main/kotlin/ui/MainView.kt
@@ -1,68 +1,29 @@
 package ui
 
 import javafx.scene.canvas.Canvas
-import javafx.scene.image.WritableImage
-import javafx.scene.paint.Color
-import tornadofx.*
+import particle.Settings
+import tornadofx.View
+import tornadofx.canvas
+import tornadofx.group
+import tornadofx.pane
 
 class MainView : View() {
 
-    val writableImage = WritableImage(100, 100)
-    var cnv: Canvas = Canvas(1.0, 1.0)
+    private var cnv: Canvas = Canvas(1.0, 1.0)
 
     override val root = pane {
         setMinSize(500.0, 500.0)
         title = "Particle Automata"
 
         group {
-            rectangle {
-                x = 25.0
-                y = 25.0
-                width = 250.0
-                height = 250.0
-                fill = Color.BEIGE
-            }
-
-            imageview {
-                x = 50.0
-                y = 50.0
-                image = writableImage
-
-            }
-
             cnv = canvas {
-                width = 100.0
-                height = 100.0
-                layoutX = 100.0
-                layoutY = 100.0
+                width = Settings.w.toDouble()
+                height = Settings.h.toDouble()
+                layoutX = 0.0
+                layoutY = 0.0
             }
+
+            Thread(Renderer(cnv.graphicsContext2D)).start()
         }
-
-        runAsync {
-//            while (true) {
-                writableImage.pixelWriter.setColor(
-                        randomX(),
-                        randomY(),
-                        Color.BLACK
-                )
-
-                with(cnv.graphicsContext2D) {
-                    fill = Color.AZURE
-                    fillRect(0.0, 0.0, 99.0, 99.0)
-                    rect(0.0, 0.0, 99.0, 99.0)
-                    fill()
-                }
-
-                try {
-                    Thread.sleep(15)
-                } catch (e: InterruptedException) {
-                    e.printStackTrace()
-                }
-            }
-//        }
     }
-
-    private fun randomY() = Math.round(Math.random() * (writableImage.height - 1)).toInt()
-
-    private fun randomX() = Math.round(Math.random() * (writableImage.width - 1)).toInt()
 }
