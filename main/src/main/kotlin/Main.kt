b996ecb68902120425a7a684a2b862d260f63122
Vladimir Lutsenko <velutsen@mts.ru>
2024-01-14 17:16:27 +0300

    Reverted back to previos rendering way

--- main/src/main/kotlin/Main.kt
+++ main/src/main/kotlin/Main.kt
@@ -1,10 +1,7 @@
 import ui.Form
-import ui.launchApp
 
 fun main() {
-//    val f = Form()
-//    f.title = "Kotlin Objects"
-//    Thread(f).start()
-
-    launchApp()
+    val f = Form()
+    f.title = "Kotlin Objects"
+    Thread(f).start()
 }
